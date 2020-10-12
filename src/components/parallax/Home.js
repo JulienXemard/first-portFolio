ba5ae74bb71f33ff76492c2879ee62ec61bc4b4e
Julien Xemard <julien.xemard@outlook.com>
2020-10-12 15:06:14 +0200

    Final --v

--- src/components/parallax/Home.js
+++ src/components/parallax/Home.js
@@ -13,7 +13,7 @@ function Home() {
 
   return (
     <>
-      <Parallax pages={3.05} ref={ref => (parallax = ref)}>
+      <Parallax pages={5.38} ref={ref => (parallax = ref)}>
 
         <nav>
           <div className="nav-box">
